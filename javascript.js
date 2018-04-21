a2b2b52c4c5605675f1b1c7806aab9bc94b2ad82
Ava <ava@starspace46.com>
2018-04-21 13:53:17 -0500

    .

--- javascript.js
+++ javascript.js
@@ -1,32 +1,31 @@
 // JavaScript Document
 var prefixL = [
-	"Would you shit your pants if I",
-	"Guess I'm one of the minority. I",
-	"Do you guys think I",
-	"What if I told you guys I",
-	"Just to clear the air for everyone, I",
-	"Is it too harsh if I",
+	"The will to win, the desire to succeed, the urge to reach your full potential...",
+	"Our greatest glory is not in never failing,",
+	"The superior man understands what is right; the inferior man",
+	"Apply yourself both now and in the next life. Without effort, you cannot be prosperous. Though the land be good, you cannot have an abundant crop without",
+	"Dignity does not consist in possessing honors, but",
+	"It is just that we should be grateful, not only to those with whose views we may agree, but also to those who have expressed",
 ];
 var phraseL = [
-	"will always support",
-	"have less than 3 miles to walk to",
-	"am ready to forgive",
-	"will accept",
-	"am not as hyped about",
-	"have to budget for",
-	"limit myself to",
-	"will sleep with",
-	"will surpass",
-	"want your honest thoughts about"
+	"dying Is MainStream #MONEY",
+	"How Can Mirrors Be Real If Our Eyes Aren't Real",
+	"If Newborn Babies Could Speak They Would Be The Most Intelligent Beings On Planet Earth",
+	"School Is The Tool To Brainwash The Youth",
+	"don't Worry Bae I'll Talk To You About SpaceTime Over FaceTime",
+	"What do I have to do to get a simpler persian rug with cherub imagery uuuuugh",
+	"fuck any game company that puts in-app purchases in kids games!!!",
+	"you may be talented but you're not kanye west",
+	"sometimes I get emotional over fonts",
+	"Every Ides of March, I order a Caesar Salad and stab it repeatedly with a knife",
+	"Curling are you serious",
 ];
 var bonusL = [
-	"It's gross.",
-	"It's impossible.",
-	"Nope.",
-	"Or whatever.",
-	"Am I the only one?",
-	"I hope that clears the air for everyone.",
-	"Screw these prices!"
+	"#blessed",
+	"#winning",
+	"#humblebrag",
+	"#quoteoftheday",
+	"#deep",
 ];
 
 function getPrefix() {
@@ -49,8 +48,8 @@ function getBonus() {
 	}
 
 function getQuote() {
-	var quote = getPrefix() +" "+ getPhrase() +" "+ getSubject() +" "+ getSuffix();
-	if(Math.random() > 0.80){
+	var quote = getPrefix() +" "+ getPhrase();
+	if(Math.random() > 0.60){
 		quote = quote + " " +getBonus();
 	}
 	var div = document.getElementById('quoteDisplay').innerHTML = quote;
